8337f66fe97c369c43a6069b934c866bee15aca5
Junaid-PK <junaidhussnain369@gmail.com>
2023-05-10 15:43:50 -0700

    Added Solution to LongestConsequtiveSequence

---
+++ LongestConsecutiveSequence/Program.py
@@ -0,0 +1,15 @@
+def longestConsecutive(nums):
+    numSet = set(nums)
+    longest = 0
+
+    for n in nums:
+        if n-1 not in numSet:
+            length = 0
+            while n+length in numSet:
+                length +=1
+            longest = max(length,longest)
+    return longest
+
+        
+
+print(longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
