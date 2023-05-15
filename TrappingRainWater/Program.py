6491304c31813dfd68d08f9db73f75d4b15d332d
Junaid-PK <junaidhussnain369@gmail.com>
2023-05-14 17:02:29 -0700

    feat: :white_check_mark: Added Solution to TrappingRainWater

---
+++ TrappingRainWater/Program.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height: return 0
+
+        l,r = 0 , len(height)-1
+        leftMax,rightMax = height[l],height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l+=1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r-=1
+                rightMax = max(rightMax,height[r])
+                res+= rightMax - height[r]
+        return res
+
+mySolution = Solution()
+print(mySolution.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
